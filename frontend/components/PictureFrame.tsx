94057a2449623b396527185c5dc78dd5e9048d32
pwilson77 <p.o.yirenkyi@gmail.com>
2022-07-17 21:38:23 +0000

    Added dynamic route for individual NFTs

--- frontend/components/PictureFrame.tsx
+++ frontend/components/PictureFrame.tsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 
 interface PictureFrameProp {
     imgUrl: string;
 }
 
-const PictureFrame = ({imgUrl}: PictureFrameProp) => {
+const PictureFrame = ({ imgUrl }: PictureFrameProp) => {
     return (
         <div className="border-solid border-red-500 my-5 mx-5 rounded-md h-[400px] overflow-hidden shadow-lg shadow-slate-800">
-            <img src={imgUrl} alt="beach holiday" className="object-cover w-full h-full"/>
+            <Link href="/philip"><img src={imgUrl} alt="beach holiday" className="object-cover w-full h-full" /></Link>
         </div>
     );
 }
