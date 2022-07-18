ed43a6082d9b5935d35e4e0773b44b40a21d7bb7
kinluek <kinluek@gmail.com>
2022-07-18 08:39:41 +0100

    add cors and dotenv

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,18 +2,31 @@ import express from 'express';
 import { IpfsClient } from './services/ipfs/IpfsClient.js';
 import { NFTContractClient } from './services/blockchain/NFTContract.js';
 import { ethers } from 'ethers';
-import 'dotenv';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS;
 const NFT_COLLECTION_SIZE = 10;
 const provider = ethers.getDefaultProvider('ropsten');
+
+console.log(`using contract address: ${NFT_CONTRACT_ADDRESS}`);
+
 const contractClient = new NFTContractClient(NFT_CONTRACT_ADDRESS, NFT_COLLECTION_SIZE, provider);
 const ipfsClient = new IpfsClient('localhost', 5001, 'http');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get('/nfts/metadata/:id', async (req, res) => {
     const tokenId = req.params.id;
+    const tokenIdInt = parseInt(tokenId, 10);
+    if (isNaN(tokenIdInt)) {
+        res.sendStatus(400).send(`id provided ${tokenId} is not a number`);
+        return;
+    }
+
     const tokenUri = await contractClient.tokenURI(parseInt(tokenId, 10));
     const metadata = await ipfsClient.getContent(tokenUri);
     res.contentType('application/json').send(metadata);
